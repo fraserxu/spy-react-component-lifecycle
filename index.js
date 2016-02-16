29215d6fb79cd89f88125f4c54f6c91f502011e2
fraserxu <xvfeng123@gmail.com>
2016-02-16 15:09:25 +1100

    add files

---
+++ index.js
@@ -0,0 +1,35 @@
+var sinon = require('sinon')
+
+function onPrototype (Component, lifecycle, method) {
+  var proto = Component.prototype
+  Object.getOwnPropertyNames(proto).forEach(function (name) {
+    if (typeof proto[name] !== 'function') return
+    switch (name) {
+      case 'componentDidMount':
+      case 'componentWillMount':
+      case 'componentDidUnmount':
+      case 'componentWillUnmount':
+      case 'componentWillReceiveProps':
+      case 'componentDidUpdate':
+      case 'componentWillUpdate':
+      case 'shouldComponentUpdate':
+      case 'render':
+        if (lifecycle) lifecycle(proto, name)
+        break
+      case 'constructor':
+        // don't spy on the constructor, even though it shows up in the prototype
+        break
+      default:
+        if (method) method(proto, name)
+        break
+    }
+  })
+}
+
+function spyLifeCycle(Component) {
+  onPrototype(Component, function(proto, name) {
+    sinon.spy(proto, name)
+  })
+}
+
+module.exports = spyLifeCycle
---
+++ test.js
@@ -0,0 +1,50 @@
+var spyLifeCycle = require('./')
+var test = require('tape')
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+function withDom () {
+  if (!global.document) {
+    try {
+      var jsdom = require('jsdom').jsdom
+
+      var exposedProperties = ['window', 'navigator', 'document']
+
+      global.document = jsdom('')
+      global.window = document.defaultView
+      Object.keys(document.defaultView).forEach((property) => {
+        if (typeof global[property] === 'undefined') {
+          exposedProperties.push(property)
+          global[property] = document.defaultView[property]
+        }
+      })
+
+      global.navigator = {
+        userAgent: 'node.js',
+      }
+    } catch (e) {
+      // jsdom is not supported
+    }
+  }
+}
+
+function noop () {}
+
+var Foo = React.createClass({
+  componentDidMount: noop,
+  render: function render () {
+    return React.createElement('div', null, 'foo')
+  }
+})
+
+test('Componenet life cycle', function (assert) {
+  withDom()
+
+  spyLifeCycle(Foo)
+  var container = window.document.createElement('div')
+  ReactDOM.render(React.createElement(Foo), container)
+
+  assert.true(Foo.prototype.componentDidMount.calledOnce, 'calls componentDidMount once')
+
+  assert.end()
+})
